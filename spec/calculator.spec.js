0f6a24734269c6eb3ec4f795f9f9b172f36c1647
Andreas Hailu <hailuand@umich.edu>
2016-08-03 21:38:15 -0400

    Initial commit

---
+++ spec/calculator.spec.js
@@ -0,0 +1,9 @@
+/**
+ * Created by andreashailu on 8/3/16.
+ */
+
+describe('calculator', function(){
+    it('should add two numbers', function(){
+        expect(add(1,2)).toBe(3);
+    })
+});
---
+++ src/calculator.js
@@ -0,0 +1,7 @@
+/**
+ * Created by andreashailu on 8/3/16.
+ */
+
+function add(x, y){
+    return x + y;
+}
